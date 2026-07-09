a5c27f089b1a6786c2bb5c46f494d30dbcc64c6a
Vidhya <149255350+vidhya-18@users.noreply.github.com>
2026-07-09 09:10:05 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxi=0
+        n=len(prices)
+        leftmin=[0]*n
+        rightmax=[0]*n
+        leftmin[0]=prices[0]
+        rightmax[n-1]=prices[n-1]
+        for i in range(1,n):
+            leftmin[i]=min(prices[i],leftmin[i-1])
+        for i in range(n-2,-1,-1):
+            rightmax[i]=max(prices[i],rightmax[i+1])  
+        for i in range(n):
+            res=rightmax[i]-leftmin[i]
+            maxi=max(maxi,res)    
+        return maxi  
